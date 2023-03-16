4b964a417fe2e45bf80275d7f5322f6638cb338a
柏毅 <47583952+pylu0923@users.noreply.github.com>
2023-03-16 13:15:54 +0000

    store application in database

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, jsonify
-from database import load_from_db, load_job_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
@@ -14,6 +15,7 @@ def list_jobs():
   JOBS = load_from_db()
   return jsonify(JOBS)
 
+
 @app.route("/job/<id>")
 def show_job(id):
   job = load_job_from_db(id)
@@ -23,6 +25,18 @@ def show_job(id):
   return render_template('jobpage.html', job=job)
 
 
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  # request.args contain information from the URL, 如果route method是post, info會存在
+  # request.forms, 用成post是因為如果輸入欄位過多, URL會變成一大串, 用post就不會在URL後面
+  # 接一串參數
+  job = load_job_from_db(id)
+  data = request.form
+  add_application_to_db(id, data)
+  # return request.args
+  return render_template('application_submitted.html',
+                         application=data,
+                         job=job)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, text
 import os
 
+
 def load_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("select * from jobs"))
@@ -9,27 +10,49 @@ def load_from_db():
     for row in result_all:
       result_dict_list.append(row._asdict())  # list內是row物件, 用._asdict可以轉成dict
   return result_dict_list
-  
+
+
 def load_job_from_db(id):
   with engine.connect() as conn:
-    result = conn.execute(
-      text(f"SELECT * FROM jobs WHERE id = {id}")
-    )
+    result = conn.execute(text(f"SELECT * FROM jobs WHERE id = {id}"))
     rows = result.all()  # result.all() == list
     if len(rows) == 0:
       return None
     else:
-      return rows[0]._asdict() # list內是row物件, 用._asdict可以轉成dict
-  
+      return rows[0]._asdict()  # list內是row物件, 用._asdict可以轉成dict
+
+
+def create_table(table_name):
+  table_list = []
+  with engine.connect() as conn:
+    result = conn.execute(text("SHOW tables"))
+    for x in result:
+      table_list.append(x[0])
+    if not table_name in table_list:
+      print("now creating table....please wait")
+      sql = f"CREATE TABLE {table_name} (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, job_id INT NOT NULL, full_name VARCHAR(255) NOT NULL,\
+           email VARCHAR(255) NOT NULL, linkedin VARCHAR(500), education VARCHAR(2000), work_experience VARCHAR(2000))"
+      
+      conn.execute(text(sql))
+
+
+def add_application_to_db(job_id, data):
+  table_name = 'applications'
+  create_table(table_name)
+  # insert into後面的value用f-string要再用''包起來
+  with engine.connect() as conn:
+    query = text(
+      f"insert into {table_name} (job_id, full_name, email, linkedin,\
+    education, work_experience) VALUES ('{job_id}', '{data['full_name']}',' {data['email']}', '{data['linkedin']}','{data['education']}', '{data['work_experience']}')")
+    conn.execute(query)
+
+
 db_connect_string = os.environ['DB_CONNECT_STRING']
 
-engine = create_engine(db_connect_string, connect_args={
-  "ssl" : {
-    "ssl_ca": "/etc/ssl/cert.pem"
-  }
-})
-
-
+engine = create_engine(db_connect_string,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
 
 # with engine.connect() as conn:
 #   result = conn.execute(text("select * from jobs"))
